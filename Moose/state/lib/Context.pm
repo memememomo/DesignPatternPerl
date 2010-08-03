eb7fe6e49a54577d87e204adaf340c289e1f81f5
memememomo <memememomo@gmail.com>
2010-08-03 15:46:51 +0900

    add moose/staet

---
+++ Moose/state/lib/Context.pm
@@ -0,0 +1,5 @@
+package Context;
+use Any::Moose '::Role';
+requires qw/set_clock change_state call_security_center record_log/;
+
+1;
---
+++ Moose/state/lib/State.pm
@@ -0,0 +1,6 @@
+package State;
+use Any::Moose '::Role';
+requires qw/do_clock do_use do_alarm do_phone/;
+
+
+1;
